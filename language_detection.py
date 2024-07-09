67690b0ea7f3db655fcd22c965a6b78323589cde
Kushagra Raina <kushagra.raina@gmail.com>
2024-07-09 16:43:39 +0100

    Language Detection Module, TODO:filetype error handling

--- language_detection.py
+++ language_detection.py
@@ -2,18 +2,23 @@ import os
 import random
 import whisper
 
-model = whisper.load_model("small")
-
-test_dir = r"D:\Projects\Datasets\Indian_Languages_Audio_Dataset"
+from pydub import AudioSegment
 
 
+def language_detector(model, fileName):
 
-for i in range(20):
-    lang_dir = os.path.join(test_dir, random.choice(os.listdir(test_dir)))
-    audio_file = os.path.join(lang_dir, random.choice(os.listdir(lang_dir)))
-    print(lang_dir.split('\\')[-1])
+    # if file[-4:] != '.mp3':
+    #     wav_audio = AudioSegment.from_file("audio.wav", format="wav")
+    #     raw_audio = AudioSegment.from_file("audio.wav", format="raw",
+    #                                        frame_rate=44100, channels=2, sample_width=2)
+    #
+    #     wav_audio.export("audio.mp3", format="mp3")
+    #     raw_audio.export("audio1.mp3", format="mp3")
+    if not fileName.endswith('.mp3'):
+        raise ValueError("File is not of type .mp3, Please ")
+    awesome = AudioSegment.from_file(fileName, "")
+    awesome.export("D:\Projects\Datasets\manjot_voice.mp3", format="mp3")
 
-    # load audio and pad/trim it to fit 30 seconds
     audio = whisper.load_audio(audio_file)
     audio = whisper.pad_or_trim(audio)
 
@@ -24,5 +29,21 @@ for i in range(20):
     _, probs = model.detect_language(mel)
     print(f"Detected language: {max(probs, key=probs.get)}")
 
-#D:\Projects\Datasets\Indian_Languages_Audio_Dataset\Punjabi
-#D:\Projects\Datasets\Indian_Languages_Audio_Dataset\Punjabi\9715.mp3
+
+## TESTS:
+
+# tests=20
+# audio_file = "D:\Projects\Datasets\manjot_voice.opus"
+# test_dir = r"D:\Projects\Datasets\Indian_Languages_Audio_Dataset"
+
+# model = whisper.load_model("small")
+
+# for i in range(tests):
+#     lang_dir = os.path.join(test_dir, random.choice(os.listdir(test_dir)))
+#     file = random.choice(os.listdir(lang_dir))
+#     audio_file = os.path.join(lang_dir, file)
+
+#     convert file type
+
+#     print(lang_dir.split('\\')[-1])
+#     language_detector(model, audio_file)
